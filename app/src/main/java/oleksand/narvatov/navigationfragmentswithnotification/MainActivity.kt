a96037b6f4b0537774336d6acfa9ae2311de5bf8
aleksandr <aleksandr.narvatov@nitrix.io>
2022-06-18 17:34:01 +0300

    Initial commit

---
+++ app/src/main/java/oleksand/narvatov/navigationfragmentswithnotification/MainActivity.kt
@@ -0,0 +1,11 @@
+package oleksand.narvatov.navigationfragmentswithnotification
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
