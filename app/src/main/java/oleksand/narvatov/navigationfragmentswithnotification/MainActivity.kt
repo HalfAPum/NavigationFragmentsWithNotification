e62e84405327b88f52f40b6e4d2990aa4cffe4c0
aleksandr <aleksandr.narvatov@nitrix.io>
2022-06-18 18:12:15 +0300

    fragment design

--- app/src/main/java/oleksand/narvatov/navigationfragmentswithnotification/MainActivity.kt
+++ app/src/main/java/oleksand/narvatov/navigationfragmentswithnotification/MainActivity.kt
@@ -2,10 +2,21 @@ package oleksand.narvatov.navigationfragmentswithnotification
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import by.kirich1409.viewbindingdelegate.viewBinding
+import oleksand.narvatov.navigationfragmentswithnotification.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding: ActivityMainBinding by viewBinding()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val fragment = NotificationCounterFragment()
+        supportFragmentManager.beginTransaction().apply {
+            add(R.id.container, fragment)
+            addToBackStack(null)
+            commit()
+        }
     }
 }
